6244d6881958fd3fa1c24ad936c6985996f8f89e
Mette Pernille Hellesvik <pernillehellesvik@gmail.com>
2012-11-06 13:27:25 +0100

    Gjorde noen endringer på footeren

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,4 +1,9 @@
 <div id="footer">
+	<!-- Disse BR-tagsa er her bare til jeg finner ut hvordan jeg 		vellykket pusher tekst til bunnen av en div -->
+	<br />
+	<br />
+	<br />
+	<br />
 	<p>©2012</p>
 	<div id="siteurl" style="display:none;">
 		<p><?php echo site_url(); ?></p>
