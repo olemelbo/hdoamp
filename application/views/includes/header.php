12156229cc81721687be9a92839f1167003ac8c9
Marte Johnsen <johnsemarte@gmail.com>
2012-11-13 11:26:45 +0100

    smallfix

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -10,6 +10,7 @@
 	<link rel="stylesheet" href="http://code.jquery.com/ui/1.9.0/themes/base/jquery-ui.css" />
 	<link rel="stylesheet" href="<?php echo base_url(); ?>css/lightbox.css" />
 	<link href='http://fonts.googleapis.com/css?family=Patrick+Hand' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Bitter' rel='stylesheet' type='text/css'>
 	<script language="javascript" src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
 	<script language="javascript" src="<?php echo base_url(); ?>js/login.js"></script>
 	<script language="javascript" src="<?php echo base_url(); ?>js/logout.js"></script>
