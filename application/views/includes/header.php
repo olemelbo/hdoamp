e9d10b0cd1d1396919fd61309cd2973faba9a91a
Ole Christian Melbostad <olemelbo1@gmail.com>
2012-11-18 12:06:18 +0100

    Har laget basen for bilde opplaster

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -24,7 +24,8 @@
 	<script language="javascript" src="<?php echo base_url(); ?>js/post_profile_picture.js"></script>
 	<script language="javascript" src="<?php echo base_url(); ?>js/comment.js"></script>
 	<script language="javascript" src="<?php echo base_url(); ?>js/delete.js"></script>
-	<script language="javascript" src="<?php echo base_url(); ?>js/jquery.fineuploader-3.0.js"></script>
+	<script src="<?php echo base_url()?>js/upload.js"></script>
+  	<script src="<?php echo base_url()?>js/ajaxfileupload.js"></script>
 	<script language="javascript" src="http://code.jquery.com/ui/1.9.0/jquery-ui.js"></script>
 	
 </head>
