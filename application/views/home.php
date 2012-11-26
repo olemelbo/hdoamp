a36a34ba97b65feb100dcb5a3692b6e47038fa66
Marte Johnsen <johnsemarte@gmail.com>
2012-11-26 15:58:12 +0100

    husker ikke

--- application/views/home.php
+++ application/views/home.php
@@ -31,7 +31,7 @@
 				<h2><a href="<?php echo site_url()?>/entire_post/loadEntirePost/<?php echo $post['id']?>"><?php  echo $post['tittel'] ?></a></h2>
 				
 				<?php if(!empty($post['date'])) : ?>
-					<h3><?php echo $post['date']; ?></h3>
+					<p class="date"><?php echo $post['date']; ?></p>
 				<?php endif; ?>
 				
 				<?php if(!empty($post['hashtags'])) : ?>
